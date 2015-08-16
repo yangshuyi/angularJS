b70ac904b7a4e45ea2af83cfc031aa4f22c60c8e
shuyi.yang <156838991@qq.com>
2015-08-16 10:59:00 +0800

    easy panel

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-//Express 入口
 //Step1: 导入相关模块
-//模块依赖
+//Step1.1: 模块依赖库
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+
 var bodyParser = require('body-parser');
 var fs = require('fs');
-
+//Step1.2: 加载路由
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+
+//Step1.3: 创建项目实例
 //在express内部，有一个函数的数组，暂时叫这个数组tasks，每来一个请求express内部会依次执行这个数组中的函数
 //function(req,res，next){//...}
 //next，是指下一个函数
@@ -22,6 +24,7 @@ var app = express();
 //Step2.1: 使用app.set 设置express内部的一些参数（options）
 // 环境变量
 // view engine setup
+// 定义模板引擎和模板文件位置 Jade EJS
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -29,11 +32,12 @@ app.set('view engine', 'jade');
 //app.use 加载用于处理http請求的middleware（中间件），当一个请求来的时候，会依次被这些 middlewares处理。
 //执行的顺序是你定义的顺序
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico'))); // 定义icon图标
+app.use(logger('dev')); //定义日志和输出级别
+app.use(bodyParser.json()); // 定义数据解析器
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
+app.use(cookieParser()); //定义cookie解析器 用法：在请求的req里获取cookies：JSON.stringify(req.cookies);
+
 app.use(function (req, res, next) {
     console.log("111");
     //数据库连接Filter
@@ -63,6 +67,11 @@ app.use(function (req, res, next) {
 //express内部有个map，对于每一种请求方法(get,post...)都有映射，每个都映射到一个 路由对象的数组
 app.use('/', index);
 app.use('/users', users);
+
+app.use('/module1', require('./routes/module1.js'));
+app.use('/module2', require('./routes/module2.js'));
+app.use('/module3', require('./routes/module3.js'));
+
 
 //处理静态资源
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ public/ngapp/common/easyui/Dialog.js
@@ -0,0 +1,68 @@
+'use strict';
+angular
+    .module("common.easyui.Panel", [])
+    .directive("easyPanel", function () {
+        return {
+            restrict: 'E',
+            scope: {
+                title: '@',
+                header: '=', //The panel header
+                collapsible: '=',  //Defines if to show collapsible button.
+                collapsed: '=', //Defines if the panel is collapsed at initialization.
+                panelCls: '@',  //Add a CSS class to the panel.
+                headCls: '@',  //Add a CSS class to the panel header.
+                bodyCls: '@', //Add a CSS class to the panel body.
+                style: '@',  //Add a custom specification style to the panel.
+                noHeader: '@' //If set to true, the panel header will not be created.
+            },
+            replace: false,
+            transclude: true,
+            controller: function($scope, $element){
+                $element.attr('head-cls','panel-heading');
+            },
+            link: function ($scope, $elem, attrs) {
+                console.log('EasyPanel');
+                //init
+                $scope.title = 'aaa';
+                $scope.header = $scope.header || null;
+                $scope.collapsible = $scope.collapsible || true;
+                $scope.collapsed = $scope.collapsed || false;
+                $scope.panelCls = $scope.panelCls || 'panel panel-default';
+                $scope.headCls = $scope.headCls || 'panel-heading';
+                attrs['body-cls'] = attrs['body-cls'] || 'panel-body';
+                $scope.style = $scope.style || '';
+                $scope.noHeader = $scope.noHeader || false;
+
+                //public method
+                $scope.toggle = function () {
+                    if ($scope.collapsed) {
+                        $scope.expand();
+                    } else {
+                        $scope.collapse();
+                    }
+                }
+
+                $scope.expand = function () {
+                    $scope.collapsed = false;
+                }
+
+                $scope.collapse = function () {
+                    $scope.collapsed = true;
+                }
+            }
+            ,
+            template: '' +
+            '<div style="{{style}}" class="{{panelCls}}">' +
+            '   <div class="{{headCls}}" style="display:flex;">' +
+            '       <div style="flex:1">{{title}}</div>' +
+            '       <div>' +
+            '           <span ng-if="collapsible" ng-click="toggle()" ng-class="{true:\'glyphicon glyphicon-triangle-top\',false:\'glyphicon glyphicon-triangle-bottom\'}[collapsed]"/>' +
+            '       </div>' +
+            '   </div>' +
+            '   <div class="{{bodyCls}}" ng-show="!collapsed">' +
+            '       <div ng-transclude>/' +
+            '   </div>' +
+            '</div>'
+            ,
+        }
+    })

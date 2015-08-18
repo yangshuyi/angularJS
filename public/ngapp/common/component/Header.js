5bdc0e55c3d337a1d3f37b4863c4b321ce46a1a2
shuyi.yang <156838991@qq.com>
2015-08-18 23:06:13 +0800

    container

--- public/ngapp/common/component/Header.js
+++ public/ngapp/common/component/Header.js
@@ -1,8 +1,21 @@
 angular.module('common.component').directive('header', ['$templateCache', function ($templateCache) {
     var defaultTmplUrl = 'template/common/component/header.html';
     $templateCache.put(defaultTmplUrl,
-        '<div class="row">' +
-        '   <div class="col-12" style="background: pink">Header</div>' +
+        '<div class="row" style="background: black;height:60px; line-height: 60px; text-align: center;">'+
+        '   <div class="col-xs-1"></div>'+
+        '   <div class="col-xs-2">'+
+        '       <div style="color:white;"> DRP </div>'+
+        '   </div>'+
+        '   <div class="col-xs-1"></div>'+
+        '   <div class="col-xs-5">' +
+        '           <ul class="nav nav-pills">'+
+    '<li role="presentation" class="active"><a href="#">Home</a></li>'+
+    '<li role="presentation"><a href="#">Profile</a></li>'+
+    '<li role="presentation"><a href="#">Messages</a></li>'+
+    '</ul>'+
+
+    '   </div>'+
+        '   <div class="col-xs-1"></div>'+
         '</div>');
 
     return {

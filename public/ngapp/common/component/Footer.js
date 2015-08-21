1f4a5ab4000f57c3bb1cdf046ae00ab4377037e7
yangshuyi <syang@cccis.com>
2015-08-21 18:29:58 +0800

    do it

--- public/ngapp/common/component/Footer.js
+++ public/ngapp/common/component/Footer.js
@@ -2,16 +2,28 @@
 angular.module('common.component').directive('footer', ['$templateCache', function ($templateCache) {
     var defaultTmplUrl = 'template/common/component/footer.html';
     $templateCache.put(defaultTmplUrl,
-        '<div class="container" style="text-align: center;border-top:1px solid grey">' +
-        '   &copy; 2015' +
-        '</div>');
+    '<div class="container">'+
+    '    <div class="row">'+
+    '       <div class="col-sm-6">&copy; 2013 <a target="_blank" href="{{url}}" title="{{footName}}">{{footName}}</a>. All Rights Reserved.</div>'+
+    '       <div class="col-sm-6">'+
+    '           <ul class="pull-right">'+
+    '               <li><a href="#">Home</a></li>'+
+    '               <li><a href="#">About Us</a></li>'+
+    '               <li><a href="#">Faq</a></li>'+
+    '               <li><a href="#">Contact Us</a></li>'+
+    '           </ul>'+
+    '       </div>'+
+    '   </div>'+
+    '</div>'
+    );
 
     return {
         restrict: 'EA',
         replace: false,
         scope: {},
         link: function (scope, el, attrs, ngModelCtrl) {
-
+            scope.url='http://shapebootstrap.net/';
+            scope.footName='Free Twitter Bootstrap WordPress Themes and HTML templates'
         },
         templateUrl: function (element, attrs) {
             return attrs.templateUrl || defaultTmplUrl;
--- public/ngapp/common/component/Header.js
+++ public/ngapp/common/component/Header.js
@@ -2,22 +2,65 @@
 angular.module('common.component').directive('header', ['$templateCache', function ($templateCache) {
     var defaultTmplUrl = 'template/common/component/header.html';
     $templateCache.put(defaultTmplUrl,
-        '<div class="row" style="background: black;height:60px; line-height: 60px; text-align: center;">'+
-        '   <div class="col-xs-1"></div>'+
-        '   <div class="col-xs-2">'+
-        '       <div style="color:white;"> DRP </div>'+
-        '   </div>'+
-        '   <div class="col-xs-1"></div>'+
-        '   <div class="col-xs-5">' +
-        '           <ul class="nav nav-pills">'+
-    '<li role="presentation" class="active"><a href="#">Home</a></li>'+
-    '<li role="presentation"><a href="#">Profile</a></li>'+
-    '<li role="presentation"><a href="#">Messages</a></li>'+
-    '</ul>'+
-
+    '<div class="top-bar">'+
+    '   <div class="container">'+
+    '       <div class="row">'+
+    '           <div class="col-sm-6 col-xs-4">'+
+    '               <div class="top-number"><p><i class="fa fa-phone-square"></i>  +0123 456 70 90</p></div>'+
+    '           </div>'+
+    '           <div class="col-sm-6 col-xs-8">'+
+    '               <div class="social">'+
+    '                   <ul class="social-share">'+
+    '                       <li><a href="#"><i class="fa fa-facebook"></i></a></li>'+
+    '                       <li><a href="#"><i class="fa fa-twitter"></i></a></li>'+
+    '                       <li><a href="#"><i class="fa fa-linkedin"></i></a></li>'+
+    '                       <li><a href="#"><i class="fa fa-dribbble"></i></a></li>'+
+    '                       <li><a href="#"><i class="fa fa-skype"></i></a></li>'+
+    '                   </ul>'+
+    '                   <div class="search">'+
+    '                       <form role="form">'+
+    '                           <input type="text" class="search-form" autocomplete="off" placeholder="Search">'+
+    '                           <i class="fa fa-search"></i>'+
+    '                       </form>'+
+    '                   </div>'+
+    '               </div>'+
+    '           </div>'+
+    '       </div>'+
     '   </div>'+
-        '   <div class="col-xs-1"></div>'+
-        '</div>');
+    '</div>'+
+    '<nav class="navbar navbar-inverse" role="banner">'+
+    '    <div class="container">'+
+    '       <div class="navbar-header">'+
+    '           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">'+
+    '               <span class="sr-only">Toggle navigation</span>'+
+    '               <span class="icon-bar"></span>'+
+    '               <span class="icon-bar"></span>'+
+    '               <span class="icon-bar"></span>'+
+    '           </button>'+
+    '           <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="logo"></a>'+
+    '       </div>'+
+    '       <div class="collapse navbar-collapse navbar-right">'+
+    '           <ul class="nav navbar-nav">'+
+    '               <li><a href="index.html">Home</a></li>'+
+    '               <li><a href="about-us.html">About Us</a></li>'+
+    '               <li><a href="services.html">Services</a></li>'+
+    '               <li class="active"><a href="portfolio.html">Portfolio</a></li>'+
+    '               <li class="dropdown">'+
+    '                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <i class="fa fa-angle-down"></i></a>'+
+    '                   <ul class="dropdown-menu">'+
+    '                       <li><a href="blog-item.html">Blog Single</a></li>'+
+    '                       <li><a href="pricing.html">Pricing</a></li>'+
+    '                       <li><a href="404.html">404</a></li>'+
+    '                       <li><a href="shortcodes.html">Shortcodes</a></li>'+
+    '                   </ul>'+
+    '               </li>'+
+    '               <li><a href="blog.html">Blog</a></li>'+
+    '               <li><a href="contact-us.html">Contact</a></li>'+
+    '           </ul>'+
+    '       </div>'+
+    '   </div>'+
+    '</nav>'
+    );
 
     return {
         restrict: 'EA',
